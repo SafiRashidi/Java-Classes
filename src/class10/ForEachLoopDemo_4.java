a82d8de602bc219f45a6311dd70f33db7e711bf5
SafiRashidi <safirashidi55@gmail.com>
2023-01-18 17:26:43 -0600

    Class 10

---
+++ src/class10/ForEachLoopDemo_4.java
@@ -0,0 +1,15 @@
+package class10;
+
+import java.util.Arrays;
+
+/*
+limitation of foreach loop
+ */
+public class ForEachLoopDemo_4 {
+    public static void main(String[] args) {
+
+        int [] arr={10,13,20,25,45,50};
+        // Bellow code prints all the elements from an array without a loop
+        System.out.println(Arrays.toString(arr));
+    }
+}
