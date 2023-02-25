aa07df40520cde00811642b7ded79c020974a6fe
SafiRashidi <safirashidi55@gmail.com>
2023-02-25 13:35:14 -0600

    Class 29

---
+++ src/class29/SetDemo3.java
@@ -0,0 +1,19 @@
+package class29;
+
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class SetDemo3 {
+    public static void main(String[] args) {
+
+        // remove duplicate values and sort based alphabetic order
+        TreeSet<String> fruit = new TreeSet<>();
+        fruit.add("Kiwi");
+        fruit.add("Mango");
+        fruit.add("Banana");
+        fruit.add("Kiwi");
+        fruit.add("Apple");
+
+        System.out.println(fruit);
+    }
+}
