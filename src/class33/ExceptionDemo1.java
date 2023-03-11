ba908a173f5f8f28ebc85d9948f62fa7f8da6108
SafiRashidi <safirashidi55@gmail.com>
2023-03-11 13:20:50 -0600

    Class 33

---
+++ src/class33/ExceptionDemo1.java
@@ -0,0 +1,18 @@
+package class33;
+
+public class ExceptionDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Line1");
+        System.out.println("Line2");
+        System.out.println("Line3");
+        System.out.println("Line4");
+        System.out.println("Line5");
+        System.out.println("Line6");
+        String name = null;
+        System.out.println(name.length());
+        System.out.println("Line7");
+        System.out.println("Line8");
+        System.out.println("Line9");
+        System.out.println("Line10");
+    }
+}
---
+++ src/class33/ExceptionDemo2.java
@@ -0,0 +1,26 @@
+package class33;
+
+public class ExceptionDemo2 {
+    public static void main(String[] args) {
+        System.out.println("Line1");
+        System.out.println("Line2");
+        System.out.println("Line3");
+        System.out.println("Line4");
+        System.out.println("Line5");
+        System.out.println("Line6");
+        try {
+            // here we put the code which might throw an error
+            String name = null;
+            System.out.println(name.length());
+        } catch (NullPointerException npe){
+            System.out.println("We are trying to access a method ona null object");
+        } catch (ArithmeticException e) {
+            System.out.println("Some one is trying to divide by 0");
+        }
+
+        System.out.println("Line7");
+        System.out.println("Line8");
+        System.out.println("Line9");
+        System.out.println("Line10");
+    }
+}
